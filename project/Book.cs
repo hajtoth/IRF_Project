7add9b516e9acb0eb350f9b16ae4a96c61487992
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-12-04 18:36:21 +0100

    create xml serializable Book class

---
+++ project/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace project
+{
+    [XmlType("book")]
+    public class Book
+    {
+        [XmlAttribute("id")]
+        public string bookId { get; set; }
+        [XmlElement(ElementName ="author")]
+        public string author { get; set; }
+
+        [XmlElement(ElementName = "title")]
+        public string title { get; set; }
+        [XmlElement(ElementName = "genre")]
+        public string genre { get; set; }
+        [XmlElement(ElementName = "price")]
+        public double price { get; set; }
+        [XmlElement(ElementName = "publish_date")]
+        public DateTime publish_date { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string description { get; set; }
+
+    }
+}
