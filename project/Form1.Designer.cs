3bc88cef30db4dcdab64222f28f16859ee13d122
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-12-10 15:27:25 +0100

    implement main form

--- project/Form1.Designer.cs
+++ project/Form1.Designer.cs
@@ -29,12 +29,142 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Display books";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "New book";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Sale";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.saleButton_Click);
+            // 
+            // exportButton
+            // 
+            this.exportButton.Location = new System.Drawing.Point(12, 165);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(87, 23);
+            this.exportButton.TabIndex = 6;
+            this.exportButton.Text = "Export";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(134, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(745, 323);
+            this.panel1.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(745, 323);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Bright", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(63, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(441, 72);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Book registry";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Bright", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(614, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "F20VLO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(561, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tóth Hajnalka Judit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(916, 393);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.BindingSource bindingSource1;
     }
 }
 
--- project/Form1.cs
+++ project/Form1.cs
@@ -3,18 +3,153 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace project
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
+
             InitializeComponent();
+            dataGridView1.Visible = false;
+
+            bindingSource1.DataSource = LoadCatalogXml();
+            dataGridView1.DataSource = bindingSource1;
+            dataGridView1.Refresh();
+
         }
+
+        private static List<Book> LoadCatalogXml()
+        {
+            List<Book> readedBooks = new List<Book>();
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Book>), new XmlRootAttribute("catalog"));
+
+            using (FileStream fs = new FileStream("catalog.xml", FileMode.Open, FileAccess.Read))
+            {
+                readedBooks = xmlSerializer.Deserialize(fs) as List<Book>;
+            }
+            return readedBooks;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            dataGridView1.Visible = true;
+
+            panel1.Controls.Add(dataGridView1);
+            dataGridView1.Refresh();
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            NewBookForm nbf = new NewBookForm();
+            DialogResult result = nbf.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                Book book = new Book();
+
+                book.author = nbf.textBox1.Text;
+                book.title = nbf.textBox2.Text;
+                book.genre = nbf.textBox3.Text;
+                book.price = double.Parse(nbf.textBox4.Text);
+                book.publish_date = DateTime.Parse(nbf.textBox6.Text);
+                book.description = nbf.textBox5.Text;
+
+                bindingSource1.Add(book);
+
+            }
+
+        }
+
+        private void saleButton_Click(object sender, EventArgs e)
+        {
+
+            List<Book> books = bindingSource1.DataSource as List<Book>;
+            Random rng = new Random();
+            foreach (Book book in books)
+            {
+                int saleCode = rng.Next(0, 10);
+                if (saleCode == 6)
+                {
+                    book.price *= 0.9;
+                }
+                else if (saleCode == 7)
+                {
+                    book.price *= 0.8;
+                }
+                else if (saleCode == 8)
+                {
+                    book.price *= 0.7;
+                }
+                else if (saleCode == 9)
+                {
+                    book.price *= 0.6;
+                }
+                else if (saleCode == 5)
+                {
+                    book.price *= 0.5;
+                }
+
+            }
+            dataGridView1.Refresh();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath; // Alapértelmezetten az exe fájlunk mappája fog megnyílni a dialógus ablakban
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv"; // A kiválasztható fájlformátumokat adjuk meg ezzel a sorral. Jelen esetben csak a csv-t fogadjuk el
+            sfd.DefaultExt = "csv"; // A csv lesz az alapértelmezetten kiválasztott kiterjesztés
+            sfd.AddExtension = true; // Ha ez igaz, akkor hozzáírja a megadott fájlnévhez a kiválasztott kiterjesztést, de érzékeli, ha a felhasználó azt is beírta és nem fogja duplán hozzáírni
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            List<Book> books = bindingSource1.DataSource as List<Book>;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                // Végigmegyünk a hallgató lista elemein
+                foreach (var s in books)
+                {
+                    // Egy ciklus iterációban egy sor tartalmát írjuk a fájlba
+                    // A StreamWriter Write metódusa a WriteLine-al szemben nem nyit új sort
+                    // Így darabokból építhetjük fel a csv fájl pontosvesszővel elválasztott sorait
+                    sw.Write(s.author);
+                    sw.Write(";");
+                    sw.Write(s.title);
+                    sw.Write(";");
+                    sw.Write(s.genre);
+                    sw.Write(";");
+                    sw.Write(s.price.ToString());
+                    sw.Write(";");
+                    sw.Write(s.publish_date.ToString());
+                    sw.Write(s.description);
+                    sw.Write(";");
+                    sw.WriteLine();
+                }
+
+
+            }
+        }
+
     }
 }
