f964ed871235911ae3755a25ca9313ed3398b31a
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-12-12 16:26:24 +0100

    create price calculator test

---
+++ CatalogTest/PriceCalculatorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using project;
+
+namespace CatalogTest
+{
+    [TestFixture]
+    public class PriceCalculatorTest
+    {
+        [TestCase(1.0, 3, 1.0)]
+        [TestCase(2.0, 5, 2.0)]
+        [TestCase(1.0, 6, 0.9)]
+        [TestCase(2.2, 10, 1.1)]
+        public void PriceTest(double price, int salecode, double expectedPrice)
+        {
+
+            double calculatedPrice = PriceCalculator.CalculateSalePrice(price, salecode);
+            Assert.AreEqual(calculatedPrice, expectedPrice);
+
+        }
+
+
+    }
+}
---
+++ project/PriceCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public static class PriceCalculator
+    {
+        public static double CalculateSalePrice(double price, int saleCode)
+        {
+            if (saleCode == 6)
+            {
+                price *= 0.9;
+            }
+            else if (saleCode == 7)
+            {
+                price *= 0.8;
+            }
+            else if (saleCode == 8)
+            {
+                price *= 0.7;
+            }
+            else if (saleCode == 9)
+            {
+                price *= 0.6;
+            }
+            else if (saleCode == 10)
+            {
+                price *= 0.5;
+            }
+
+            return Math.Round(price, 2, MidpointRounding.AwayFromZero);
+        }
+        
+    }
+}
