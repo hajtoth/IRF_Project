070564426d39b332a9ed8cd7389e5c5dd790b1f6
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-12-14 17:05:52 +0100

    adding new book generate id itself

--- project/Form1.Designer.cs
+++ project/Form1.Designer.cs
@@ -29,142 +29,190 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.newBookButton = new System.Windows.Forms.Button();
+            this.saleButton = new System.Windows.Forms.Button();
             this.exportButton = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.catalog = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            this.panel1.SuspendLayout();
+            this.bindingSource2 = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 34);
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(18, 178);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 23);
-            this.button1.TabIndex = 3;
+            this.button1.Size = new System.Drawing.Size(153, 77);
+            this.button1.TabIndex = 4;
             this.button1.Text = "Display books";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // newBookButton
             // 
-            this.button2.Location = new System.Drawing.Point(12, 75);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "New book";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.newBookButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.newBookButton.Enabled = false;
+            this.newBookButton.Font = new System.Drawing.Font("Lucida Sans Typewriter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newBookButton.Location = new System.Drawing.Point(18, 297);
+            this.newBookButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.newBookButton.Name = "newBookButton";
+            this.newBookButton.Size = new System.Drawing.Size(153, 77);
+            this.newBookButton.TabIndex = 5;
+            this.newBookButton.Text = "New book";
+            this.newBookButton.UseVisualStyleBackColor = false;
+            this.newBookButton.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // saleButton
             // 
-            this.button3.Location = new System.Drawing.Point(12, 120);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(87, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Sale";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.saleButton_Click);
+            this.saleButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.saleButton.Enabled = false;
+            this.saleButton.Font = new System.Drawing.Font("Lucida Sans Typewriter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saleButton.Location = new System.Drawing.Point(18, 411);
+            this.saleButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.saleButton.Name = "saleButton";
+            this.saleButton.Size = new System.Drawing.Size(153, 77);
+            this.saleButton.TabIndex = 6;
+            this.saleButton.Text = "Sale";
+            this.saleButton.UseVisualStyleBackColor = false;
+            this.saleButton.Click += new System.EventHandler(this.saleButton_Click);
             // 
             // exportButton
             // 
-            this.exportButton.Location = new System.Drawing.Point(12, 165);
+            this.exportButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.exportButton.Enabled = false;
+            this.exportButton.Font = new System.Drawing.Font("Lucida Sans Typewriter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exportButton.Location = new System.Drawing.Point(18, 543);
+            this.exportButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.exportButton.Name = "exportButton";
-            this.exportButton.Size = new System.Drawing.Size(87, 23);
-            this.exportButton.TabIndex = 6;
+            this.exportButton.Size = new System.Drawing.Size(153, 77);
+            this.exportButton.TabIndex = 7;
             this.exportButton.Text = "Export";
-            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.UseVisualStyleBackColor = false;
             this.exportButton.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.dataGridView1);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(134, 24);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(745, 323);
-            this.panel1.TabIndex = 7;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(161)))), ((int)(((byte)(161)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(225, 65);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(745, 323);
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(1182, 497);
             this.dataGridView1.TabIndex = 8;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Lucida Bright", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(63, 39);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(441, 72);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Book registry";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Lucida Bright", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(614, 282);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "F20VLO";
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(561, 252);
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1280, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(162, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Tóth Hajnalka Judit";
+            this.label1.Size = new System.Drawing.Size(82, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(954, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // catalog
+            // 
+            this.catalog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.catalog.AutoSize = true;
+            this.catalog.BackColor = System.Drawing.Color.Transparent;
+            this.catalog.Font = new System.Drawing.Font("Lucida Sans Typewriter", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.catalog.Location = new System.Drawing.Point(728, 398);
+            this.catalog.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.catalog.Name = "catalog";
+            this.catalog.Size = new System.Drawing.Size(679, 164);
+            this.catalog.TabIndex = 11;
+            this.catalog.Text = "Catalog";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(18, 65);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 77);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Main page";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(916, 393);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.exportButton);
-            this.Controls.Add(this.button3);
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1615, 796);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.catalog);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.saleButton);
+            this.Controls.Add(this.newBookButton);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Catalog";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button newBookButton;
+        private System.Windows.Forms.Button saleButton;
         private System.Windows.Forms.Button exportButton;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DataGridView dataGridView1;
         public System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.BindingSource bindingSource2;
+        private System.Windows.Forms.Label catalog;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- project/Form1.cs
+++ project/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -14,18 +15,44 @@ using System.Xml.Serialization;
 namespace project
 {
     public partial class Form1 : Form
-    {
+    { 
+        private int LastBookIdNumber { get; set; }
 
         public Form1()
         {
 
             InitializeComponent();
+            this.BackColor = Color.LimeGreen;
+            this.TransparencyKey = Color.LimeGreen;
+           
+
+            timer1.Start();
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-EN"); ;
+
+            label2.Text = DateTime.Now.ToLongDateString();
+
             dataGridView1.Visible = false;
+            
+
 
             bindingSource1.DataSource = LoadCatalogXml();
+            LastBookIdNumber = GetMaxId(bindingSource1.DataSource as List<Book>);
             dataGridView1.DataSource = bindingSource1;
             dataGridView1.Refresh();
 
+        }
+
+        private int GetMaxId(List<Book> books)
+        {
+            int[] ids = new int[books.Count];
+            int i = 0;
+            foreach (var book in books)
+            {
+                ids[i] = int.Parse(book.BookId.Replace("bk", ""));
+                i += 1;
+            }
+            return ids.Max();
         }
 
         private static List<Book> LoadCatalogXml()
@@ -42,21 +69,21 @@ namespace project
 
         private void button1_Click(object sender, EventArgs e)
         {
-            panel1.Controls.Clear();
+            catalog.Visible = false;
             dataGridView1.Visible = true;
 
-            panel1.Controls.Add(dataGridView1);
+
             dataGridView1.Refresh();
 
-
-
-
+            saleButton.Enabled = true;
+            exportButton.Enabled = true;
+            newBookButton.Enabled = true;
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            catalog.Visible = false;
 
             NewBookForm nbf = new NewBookForm();
             DialogResult result = nbf.ShowDialog();
@@ -64,7 +91,7 @@ namespace project
             if (result == DialogResult.OK)
             {
                 Book book = new Book();
-
+                book.BookId = "bk" + (++LastBookIdNumber);
                 book.Author = nbf.authorTextBox.Text;
                 book.Title = nbf.titleTextBox.Text;
                 book.Genre = nbf.genreTextBox.Text;
@@ -80,40 +107,26 @@ namespace project
 
         private void saleButton_Click(object sender, EventArgs e)
         {
+            catalog.Visible = false;
 
             List<Book> books = bindingSource1.DataSource as List<Book>;
             Random rng = new Random();
             foreach (Book book in books)
             {
                 int saleCode = rng.Next(0, 10);
-                if (saleCode == 6)
-                {
-                    book.Price *= 0.9;
-                }
-                else if (saleCode == 7)
-                {
-                    book.Price *= 0.8;
-                }
-                else if (saleCode == 8)
-                {
-                    book.Price *= 0.7;
-                }
-                else if (saleCode == 9)
-                {
-                    book.Price *= 0.6;
-                }
-                else if (saleCode == 5)
-                {
-                    book.Price *= 0.5;
-                }
+                double newPrice = PriceCalculator.CalculateSalePrice(book.Price, saleCode);
+                book.Price = newPrice;
 
             }
+
             dataGridView1.Refresh();
 
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            catalog.Visible = false;
+
             SaveFileDialog sfd = new SaveFileDialog();
 
             sfd.InitialDirectory = Application.StartupPath; // Alapértelmezetten az exe fájlunk mappája fog megnyílni a dialógus ablakban
@@ -126,6 +139,25 @@ namespace project
             Export.ExportToCsv(sfd.FileName, bindingSource1.DataSource as List<Book>);
 
 
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            label1.Text = DateTime.Now.ToLongTimeString();
+            timer1.Start();
+
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = false;
+            catalog.Visible = true;
+            saleButton.Enabled = false;
+            exportButton.Enabled = false;
+            newBookButton.Enabled = false;
 
         }
     }
