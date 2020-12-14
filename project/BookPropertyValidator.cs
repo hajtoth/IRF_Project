67fe0b43661841578e4cdd982d23af69b096afc1
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-12-14 16:59:00 +0100

    modify regex in BookPropertyValidator

--- project/BookPropertyValidator.cs
+++ project/BookPropertyValidator.cs
@@ -18,7 +18,7 @@ namespace project
 
         public static bool ValidateDate(string publish_date)
         {
-            Regex regex = new Regex("([0-9]{4})[^0-9]*([0-9]+)[^0-9]*([0-9]+)");
+            Regex regex = new Regex("^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\\d\\d$");
             return regex.IsMatch(publish_date);
 
         }
