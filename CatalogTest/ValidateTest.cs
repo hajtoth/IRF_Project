44c68c92ba6e3de39153824294e5c480fb9507dc
Hajnalka Judit Tóth <toth.hajni97@gmail.com>
2020-12-14 17:06:46 +0100

    fix date validate test's testcases

--- CatalogTest/ValidateTest.cs
+++ CatalogTest/ValidateTest.cs
@@ -25,19 +25,19 @@ namespace CatalogTest
             Assert.AreEqual(result, expectedResult);
         }
 
-        [TestCase("2020. 12. 11.", true)]
-        [TestCase("2020.12.11.", true)]
+        [TestCase("11/12/2020", true)]
+        [TestCase("2020. 12. 11.", false)]
+        [TestCase("2020.12.11.", false)]
         [TestCase("20.12.11.", false)]
-        [TestCase("20.dec.11.", false)]
-        [TestCase("2020.", false)]
-        [TestCase("12.11.", false)]
-        [TestCase("12. 11.", false)]
+        [TestCase("2020-11-12", false)]
+        [TestCase("2020-12-11", false)]
+        [TestCase("11/dec/2020", false)]
+
         public void DateValidateTest(string publish_date, bool expectedResult)
         {
             bool result = BookPropertyValidator.ValidateDate(publish_date);
             Assert.AreEqual(result, expectedResult);
         }
-
 
     }
 
